269614a5d4eed90c3da2a5f380348c9c9f2879f6
ElizavetaBB <56157675+ElizavetaBB@users.noreply.github.com>
2021-03-09 18:15:30 +0700

    Update comparator_tb.v
    More test values added

--- comparator_tb.v
+++ comparator_tb.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 03/08/2021 12:31:05 AM
+// Create Date: 03/09/2021 05:33:54 AM
 // Design Name: 
 // Module Name: comparator_tb
 // Project Name: 
@@ -21,37 +21,45 @@
 
 
 module comparator_tb;
-
-    reg a0_in, a1_in, a2_in, a3_in, b0_in, b1_in, b2_in, b3_in ;
+    reg [3:0] A;
+    reg [3:0] B;
     wire a_bigger_out, b_bigger_out, equals_out ;
     
     comparator comp(
-        .a0(a0_in),
-        .a1(a1_in),
-        .a2(a2_in),
-        .a3(a3_in),
-        .b0(b0_in),
-        .b1(b1_in),
-        .b2(b2_in),
-        .b3(b3_in),
+        .a0(A[0]),
+        .a1(A[1]),
+        .a2(A[2]),
+        .a3(A[3]),
+        .b0(B[0]),
+        .b1(B[1]),
+        .b2(B[2]),
+        .b3(B[3]),
         .a_bigger(a_bigger_out),
         .b_bigger(b_bigger_out),
         .equals(equals_out)
     );
     
-    initial begin 
+    integer i;
+    reg [7:0] test_val;
+    reg expected_val_a_bigger;
+    reg expected_val_b_bigger;
+    reg expected_val_equals;
     
-        a0_in = 1;
-        a1_in = 0;
-        a2_in = 0;
-        a3_in = 1;
-        
-        b0_in = 1;
-        b1_in = 0;
-        b2_in = 0;
-        b3_in = 1;
-       
+    initial begin
+        for (i=0;i<64;i=i+1) begin
+            test_val=i;
+            A=test_val[7:4];
+            B=test_val[3:0]; 
+            expected_val_a_bigger=A>B;
+            expected_val_b_bigger=A<B;
+            expected_val_equals=A==B;
+            #10 
+            if (a_bigger_out==expected_val_a_bigger && b_bigger_out==expected_val_b_bigger && equals_out==expected_val_equals) begin
+                $display("The comparator output is correct. A=%b, B=%b, a_bigger_out=%b,b_bigger_out=%b,equals_out=%b",A,B,a_bigger_out,b_bigger_out,equals_out);
+            end else begin
+                $display("The comparator output is wrong. A=%b, B=%b, a_bigger_out=%b,b_bigger_out=%b,equals_out=%b",A,B,a_bigger_out,b_bigger_out,equals_out);
+            end
+         end
       #10 $stop;
     end 
-
 endmodule
